f16762ef0ba0cf48d14b57e9b71b39126f7f5b7e
Renan Pallin <renanpallin@gmail.com>
2016-11-17 20:25:53 -0200

    Exercícios de triângulos simples

---
+++ ExerciciosSimples.java
@@ -0,0 +1,18 @@
+class ExerciciosSimples{
+	/*
+	 * Criar um método que receba os 3 lados de um triângulo e retorne
+	 * o tipo de triângulo (Isóceles, Escaleno ou Equilátero) 
+	 */
+	 public static String tipoDoTriangulo(double lado1, double lado2, double lado3){
+
+	 }
+
+
+	/* Começando com arrays */
+	/* Mesmo do anterior, mas agora o método deve receber um array
+	 * onde cada cada lacuna é um lado do triângulo
+	 */
+	 public static String tipoDoTrianguloPorArray(double[] lados){
+	 	
+	 }
+}
